7e3aadc4ec7356b54d9662c26a6baab2ecab94c6
Khadija Daruwala <khadija.genius@gmail.com>
2025-09-17 11:27:41 -0400

    First commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Khadija Daruwala on 2025-09-17.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
