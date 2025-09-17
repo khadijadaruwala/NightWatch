ca155e0adb7353d4ba4ccc6384fab8d2649eac08
Khadija Daruwala <khadija.genius@gmail.com>
2025-09-17 13:07:07 -0400

    Added styling and icons

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         HStack{
             VStack(alignment: .leading) {
-                Text("Nightly Tasks")
+                Text("\(Image(systemName: "moon.stars")) Nightly Tasks").font(.title3).fontWeight(.heavy).foregroundStyle(.yellow).textCase(.uppercase).underline()
                 Text("Check all windows")
                 Text("Check all doors")
                 Text("Check that the safe is loocked")
@@ -19,19 +19,22 @@ struct ContentView: View {
                 Text("Inspect security cameras")
                 Text("Clear ice from sidewalks")
                 Text("Document \"strange and unusual \" occurences")
-                Text("Weekly Tasks")
+                Text("\(Image(systemName: "sunset")) Weekly Tasks")
+                    .font(.title3).fontWeight(.heavy).foregroundStyle(.yellow).textCase(.uppercase).underline().padding(.top)
                 Text("Check inside all vacant rooms")
                 Text("Walk the perimeter of the property")
-                Text("Monthly Tasks")
+                Text("\(Image(systemName: "calendar")) Monthly Tasks")
+                    .font(.title3).font(.title3).fontWeight(.heavy).foregroundStyle(.yellow).textCase(.uppercase).underline().padding(.top)
                 Text("Test security alarms")
                 Text("Test motion detectors")
                 Text("Test smoke alarms")
                 Spacer()
-            }
+            }.foregroundStyle(.gray)
             Spacer()
-        }
+        }.padding(.all)
     }
 }
+
 
 #Preview {
     ContentView()
